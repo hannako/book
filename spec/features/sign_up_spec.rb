56c242f511472198d6b685ae1cb3d5b2164169a9
hannako <jessjones1@gmail.com>
2016-07-29 09:56:52 +0100

    step 22: complete

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -21,7 +21,7 @@ feature 'Creating user account' do
 
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content "Password and confirmation password do not match"
+    expect(page).to have_content "Password does not match the confirmation"
   end
 
 
